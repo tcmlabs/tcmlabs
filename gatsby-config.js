77e0f03aa265e2405fa10516cda21ea90211a4f7
jbmusso <jbmusso+github@gmail.com>
2018-03-10 18:20:05 +0100

    Generate Netlify _headers and _redirects files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     siteName: 'TCMLabs.fr',
   },
-  plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-styled-components'],
+  plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-styled-components', 'gatsby-plugin-netlify'],
 };
