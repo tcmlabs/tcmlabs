8bf4e1c4fde3f9ee12a041ad463f3e919a3cd7f1
Guillaume Cièr <g@creaforge.fr>
2018-03-27 19:59:47 +0200

    first design mockup

---
+++ src/components/Button.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import ContentWrapper from "./ContentWrapper";
+
+export default function Footer () {
+  return (
+    <FooterWrapper>
+			<ContentWrapper>
+
+				<FooterColumn>
+					<h3>Contact</h3>
+					<address>
+						128 rue de la boetie
+						75008 Paris
+					</address>
+					<a href="mailto:contact@tcmlabs.fr">contact@tcmlabs.fr</a>
+				</FooterColumn>
+
+				<FooterColumn>
+					<h3>Social</h3>
+					<ul>
+						<li>Twitter</li>
+						<li>Likedin</li>
+						<li>Github</li>
+						<li>Medium</li>
+					</ul>
+				</FooterColumn>
+
+			</ContentWrapper>
+    </FooterWrapper>
+  )
+}
+
+const FooterWrapper = styled.div`
+	display: flex;
+	margin-top: 200px;
+	padding: 100px 0;
+	background-color: #132533;
+`;
+
+const FooterColumn = styled.div`
+	flex: 1;
+`;
--- src/components/Typography.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.h1`
-  font-size: 43px;
-`;
-
-export const SubTitle = styled.h3`
-  font-size: 14px;
-`;
