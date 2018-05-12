ea05d64f94d7f216f4ee9b6829df7acd4db1a9e7
jbmusso <jbmusso+github@gmail.com>
2018-05-12 17:41:48 +0200

    IT expertise out of the jungle.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,10 +25,7 @@ class Index extends React.Component {
         <Callout>
           <ContentWrapper>
             <Logo />
-            <Introduction>
-              Something for your mind, your body and your soul! Stay See tradition continues for the last day of the
-              year and let me wish you all the best in 2018! #danceallnight #nylounge
-            </Introduction>
+            <Introduction>IT expertise out of the jungle.</Introduction>
             <a href={`#${FORM_NAME}`}>
               <Button>Rejoignez-nous</Button>
             </a>
@@ -75,7 +72,7 @@ const Callout = styled.div`
 		url('${calloutBackground}');
 	background-size: 100%, 30%;
   text-align: center;
-	padding: 	200px 0;
+	padding: 240px 0;
 	color: #ffffff;
 `;
 
