0f58915efc8a9054b8c8e6fc86951c0fe02ad68f
Jean-Baptiste Musso <jeanbaptiste@tcmlabs.fr>
2019-05-31 15:07:30 +0200

    Resolve tsconfig.json paths

--- tcmlabs.fr/gatsby-node.js
+++ tcmlabs.fr/gatsby-node.js
@@ -1,3 +1,15 @@
-// const Team = require('./src/team/gatsby-node');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 
-// exports.createPages = Team.createPages;
+exports.onCreateWebpackConfig = function({ actions }) {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        // This should be handled by gatsby-typescript-plugin
+        // See: https://github.com/gatsbyjs/gatsby/issues/4357
+        '#components': path.resolve(__dirname, 'src/components'),
+        '#tcmlabs.fr': path.resolve(__dirname, 'src'),
+      },
+    },
+  });
+};
