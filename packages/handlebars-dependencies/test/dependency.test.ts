1fedf9abd474a25b264297853bcd31b1b7e4426d
Jean-Baptiste Musso <jbmusso+github@gmail.com>
2019-06-15 22:24:57 +0200

    Bootstrap handlebars-dependencies package (#17)
    * Create VS Code workspace
    
    * handlebars-dependencies
    
    * Bootstrap typescript module
    
    * Check for missing/existing dependencies
    
    * Simple doc in README.md file
    
    * Build package with tsdx
    
    * Scope package under @tcmlabs namespace
    
    * Remove ts-jest devDependency
    
    * Move eslint and prettier rules in ./packages

---
+++ packages/handlebars-dependencies/test/dependency.test.ts
@@ -0,0 +1,71 @@
+import * as HandlebarsAnalyser from '../src';
+
+describe('Dependency graph', () => {
+  test('Retrieve a single template dependencies', () => {
+    const template = `
+      {{ foo }}
+      {{ baz.duh }}
+      {{ missing.quxProperty }}
+    `;
+
+    const dependencies = HandlebarsAnalyser.templateDependencies(template);
+
+    expect(dependencies).toMatchSnapshot();
+  });
+
+  test('Retrieve multiple template names', () => {
+    const templates = [{ name: 'foo', content: '{{ bar }}' }];
+
+    const components = HandlebarsAnalyser.templateNames(templates);
+
+    expect(components).toMatchSnapshot();
+  });
+
+  test('Retrieve template existing dependencies', () => {
+    const template = `
+      {{ foo }}
+      {{ baz.duh }}
+      {{ missing.quxProperty }}
+    `;
+
+    const templates = [{ name: 'foo', content: '{{ bar }}' }];
+
+    const existingDependencies = HandlebarsAnalyser.existingDependencies(templates, template);
+
+    expect(existingDependencies).toMatchSnapshot();
+  });
+
+  test('Retrieve template missing dependencies', () => {
+    const template = `
+      {{ foo }}
+      {{ baz.duh }}
+      {{ missing.quxProperty }}
+    `;
+
+    const templates = [
+      { name: 'foo', content: '{{ bar }}' },
+      { name: 'baz', content: '{{ qux }}' },
+    ];
+
+    const missingDependencies = HandlebarsAnalyser.missingDependencies(templates, template);
+
+    expect(missingDependencies).toMatchSnapshot();
+  });
+
+  test('Analyse dependencies', () => {
+    const template = `
+      {{ foo }}
+      {{ baz.duh }}
+      {{ missing.quxProperty }}
+    `;
+
+    const templates = [
+      { name: 'foo', content: '{{ bar }}' },
+      { name: 'baz', content: '{{ qux }}' },
+    ];
+
+    const analysedDependencies = HandlebarsAnalyser.markDependencyPresences(templates, template);
+
+    expect(analysedDependencies).toMatchSnapshot();
+  });
+});
